00ceb289d8f23a2121ade3f5ae7c873d6e5fbfc7
Rintze M. Zelle <rintze.zelle@gmail.com>
2015-02-03 10:34:45 -0500

    Update rspec tests with transpec

--- spec/locales_spec.rb
+++ spec/locales_spec.rb
@@ -3,40 +3,40 @@ Locales.each_pair do |id, (filename, path, locale)|
   describe "#{id}" do
 
     it "is a valid CSL 1.0.1 locale" do
-      CSL.validate(path).should == []
+      expect(CSL.validate(path)).to eq([])
     end
 
     it "has a conventional file name" do
-      filename.should match(/^locales-[a-z]{2}(-[A-Z]{2})?\.xml$/)
+      expect(filename).to match(/^locales-[a-z]{2}(-[A-Z]{2})?\.xml$/)
     end
 
     it "was successfully parsed" do
-      locale.should be_a(CSL::Locale)
+      expect(locale).to be_a(CSL::Locale)
     end
 
     unless locale.nil?
       it "has an info element" do
-       locale.should have_info
+       expect(locale).to have_info
       end
 
       it "has a language" do
-        locale.language.should_not be_empty
+        expect(locale.language).not_to be_empty
       end
 
       it "has a region" do
-        locale.region.should_not be_empty
+        expect(locale.region).not_to be_empty
       end unless NO_REGIONS.include?(locale.language.to_s)
 
       it "its language and region match the filename" do
-        locale.to_s.should == id[8,5]
+        expect(locale.to_s).to eq(id[8,5])
       end
 
       it "has and info/rights element" do
-        locale.info.should have_rights
+        expect(locale.info).to have_rights
       end
 
       it "is licensed under a CC BY-SA license" do
-        locale.info.should be_default_license
+        expect(locale.info).to be_default_license
       end
     end
 
